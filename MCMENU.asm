57ef1777dae741b2ecb9091403a1b2a90f30dd3d
c4ooo <kuznetsov4000@gmail.com>
2018-06-13 20:28:03 -0400

    working on it

---
+++ MCMENU.asm
@@ -0,0 +1,8 @@
+; convpng v6.8
+MCMENU: ; offsets
+ dl 0 ; button
+ dl 5282 ; button_down
+ dl 10564 ; mcLogo1
+ dl 16191 ; mcLogo2
+ dl 22763 ; tileRepeat
+ dl 23021 ; MCMENU
---
+++ MCTILES.asm
@@ -0,0 +1,4 @@
+; convpng v6.8
+MCTILES: ; offsets
+ dl 0 ; tiles
+ dl 16384 ; MCTILES
